d1092d489afc1d29fb80f6213c983a1e6e3fac5e
1RO8s <saki0220hiro@gmail.com>
2024-02-01 04:02:15 +0900

    コードフォーマット

--- tetris.c
+++ tetris.c
@@ -1,22 +1,23 @@
-# include "tetris.h"
+#include "tetris.h"
 
-int main() {
-	t_game game;
+int	main(void)
+{
+	t_game	game;
+
 	init_game(&game);
-  srand(time(0));
-  initscr();
+	srand(time(0));
+	initscr();
 	timeout(1);
-	if(!is_valid_position(&game,game.current)){
+	if (!is_valid_position(&game, game.current))
 		game.is_playing = FALSE;
-	}
 	set_active_block_position(&game);
-  update_screen(&game);
-	while(game.is_playing){
+	update_screen(&game);
+	while (game.is_playing){
 		operate_active_block(&game);
 		falling_active_block(&game);
 	}
 	delete_shape(game.current);
 	endwin();
 	print_result(&game);
-  return 0;
+	return (0);
 }
